a5d41ee8258e3d40aa80d49203331211b94ab8c9
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:51:31 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,8 +8,8 @@
 namespace Aurora\Modules\MailHiddenRecipientPlugin;
 
 /**
- * This module adds ability to login to the admin panel as a Super Administrator.
- *
+ * With this plugin enabled and configured, all email messages sent out will also be delivered to a predefined email address or a list of those.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
